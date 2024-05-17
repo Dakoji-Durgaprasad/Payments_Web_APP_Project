4b0526e3266b453d9a3ea285d3727ab00ee1f935
Dakoji Durgaprasad <155511435+Dakoji-Durgaprasad@users.noreply.github.com>
2024-05-17 11:04:51 +0530

    Update BankAcctFormServlet.java

--- PaymentsWebApp/src/main/java/com/pack/servs/BankAcctFormServlet.java
+++ PaymentsWebApp/src/main/java/com/pack/servs/BankAcctFormServlet.java
@@ -8,6 +8,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import com.pack.dao.PaymentsWebAppDAO;
 import com.pack.dto.BankAccount;
@@ -16,46 +17,61 @@ import com.pack.dto.User;
 @WebServlet("/BankAcctFormServlet")
 public class BankAcctFormServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-  
-    public BankAcctFormServlet() {
-        super();
-    }
 
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
+	public BankAcctFormServlet() {
+		super();
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
 		String accountNumber = request.getParameter("bankAcctNo");
 		String acctHolderName = request.getParameter("AcctHolderName");
 		String bankName = request.getParameter("bankName");
 		String bankIFSCCode = request.getParameter("bankIfscCode");
 		String bankAcctType = request.getParameter("bankAcctType");
-		String phoneNumber = request.getParameter("ph_num");
+		long phoneNumber = Long.parseLong(request.getParameter("ph_num"));
 		String bankAcctPin = request.getParameter("bankAcctPin");
 		String userName = request.getParameter("username");
-		
-		
-		PaymentsWebAppDAO dao = new PaymentsWebAppDAO();
-//		User u = new User();
-//		int currUserId = u.getUserId();
-		
-		BankAccount ba = new BankAccount();
-		
-		ba.setBankAcctNum(accountNumber);
-		ba.setBankAcctHolderName(acctHolderName);
-		ba.setBankName(bankName);
-		ba.setBankIfscCode(bankIFSCCode);
-		ba.setBankAcctType(bankAcctType);
-		ba.setBankAcctPin(bankAcctPin);
-//		ba.setUserId(currUserId);
-	
-		try {
-			dao.storeUserBankDetails(ba);
-		}catch(Exception e) {
-			e.printStackTrace();
-		}
-		response.setContentType("text/html");
-//		response.getWriter().write("<p style='color:green;'>Account Added Successful!<p>");
-		RequestDispatcher rd = request.getRequestDispatcher("/dashboard.jsp");
-		rd.include(request, response);
-	}
 
+		HttpSession session = request.getSession();
+		User sessionUser = (User) session.getAttribute("user");
+
+		if (sessionUser != null) {
+
+			PaymentsWebAppDAO dao = new PaymentsWebAppDAO();
+
+			int currUserId = sessionUser.getUserId();
+
+			BankAccount ba = new BankAccount();
+
+			ba.setBankAcctNum(accountNumber);
+			ba.setBankAcctHolderName(acctHolderName);
+			ba.setBankName(bankName);
+			ba.setBankIfscCode(bankIFSCCode);
+			ba.setBankAcctType(bankAcctType);
+			ba.setBankAcctPin(bankAcctPin);
+			ba.setPhoneNumber(phoneNumber);
+			ba.setUserId(currUserId);
+
+			System.out.println(bankAcctType);
+			System.out.println(currUserId);
+
+			try {
+				dao.storeUserBankDetails(ba);
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+			response.setContentType("text/html");
+//			response.getWriter().write("<p style='color:green;'>Account Added Successful!<p>");
+			RequestDispatcher rd = request.getRequestDispatcher("/dashboard.jsp");
+			rd.include(request, response);
+		} 
+//		else {
+//			response.setContentType("text/html");
+//			response.getWriter().write("<p style='color:red;'>Invalid Credentials</p>");
+//			RequestDispatcher rd = request.getRequestDispatcher("/index.jsp");
+//			rd.include(request, response);
+//		}
+	}
 }
